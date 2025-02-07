15cd9a8905c51065fdbad1da36cf2133e3a50d92
henrique <henriquebaslaet@hotmail.com>
2025-02-07 15:40:14 -0300

    Created my class Service

---
+++ src/main/java/com/laet/customer/service/CustomerService.java
@@ -0,0 +1,46 @@
+package com.laet.customer.service;
+
+import com.laet.customer.entity.Customer;
+import com.laet.customer.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public List<Customer> getAllCustomer() {
+        return customerRepository.findAll();
+    }
+
+    public Optional<Customer> getAllById(Long id) {
+        return customerRepository.findById(id);
+    }
+
+    public Customer createCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public Customer updateCustomer(Long id, Customer customer) {
+        if(customerRepository.existsById(id)) {
+            customer.setId(id);
+            customerRepository.save(customer);
+            return customer;
+        } else {
+            return null;
+        }
+    }
+
+    public boolean deleteCustomer(Long id) {
+        if (customerRepository.existsById(id)) {
+            customerRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
