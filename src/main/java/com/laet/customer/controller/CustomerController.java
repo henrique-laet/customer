847f5e54fffb1e481a932327f3cd5d835de12af1
henrique <henriquebaslaet@hotmail.com>
2025-02-08 01:14:15 -0300

    Implements CustomerController

---
+++ src/main/java/com/laet/customer/controller/CustomerController.java
@@ -0,0 +1,47 @@
+package com.laet.customer.controller;
+
+import com.laet.customer.entity.Customer;
+import com.laet.customer.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping
+    public List<Customer> getAllCustomer(){
+        return customerService.getAllCustomer();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getById(@PathVariable Long id){
+        Optional<Customer> customer = customerService.getAllById(id);
+        return customer.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.badRequest().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Customer> createdCustomer(@RequestBody Customer customer) {
+        Customer createdCustomer = customerService.createCustomer(customer);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdCustomer);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable Long id,@RequestBody Customer customer) {
+        Customer updateCustomer = customerService.updateCustomer(id, customer);
+        return updateCustomer != null ? ResponseEntity.ok(updateCustomer) : ResponseEntity.badRequest().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable Long id) {
+        return customerService.deleteCustomer(id) ? ResponseEntity.noContent().build() : ResponseEntity.badRequest().build();
+    }
+}
